5265c0a3f4097c5e8fa940e4bc953d5c8af82548
laura.dondiego <lauradondiego@gmail.com>
2019-07-22 15:45:21 -0400

    completed all parts of today's project and am going to move on to the next one

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React from "react";
 import "./App.css";
 import { Home, About, Contact, Navigation, BrowserRouter } from "./components";
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 const App = () => (
   <div>
     <Navigation />
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/contact">Contact</Link>
-      </li>
-    </ul>
     <Switch>
       <Route path="/" exact component={Home} />
-      {/* <Route path="/kitten/:count" component={() => <Kitten count={3} />} /> */}
       <Route path="/about" component={About} />
       <Route path="/contact" component={Contact} />
-      {/* <Route path="/kitten/" component={Kitten} /> */}
     </Switch>
   </div>
 );
